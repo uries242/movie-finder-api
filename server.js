72f3ab5b487c21e52846a4041394e17f012e6730
uries242 <teykoa@gmail.com>
2026-05-14 23:53:20 -0400

    Completed Task 4, fixed dependency and import bugs

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
-const axios = require("axios");
-require("dotenv").config();
 const PORT = process.env.PORT;
+const routes = require("./routes/movieRoutes.js");
+
+app.get("/test", (req, res) => {
+  res.send("Good Test!");
+});
+
+app.use("/api/movies", routes);
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`Listening on Port: ${PORT}`);
+});
