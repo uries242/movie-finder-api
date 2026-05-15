e43bd565536829f4cc4d4bfe648c91f28edda9f8
uries242 <teykoa@gmail.com>
2026-05-15 01:24:05 -0400

    Added validation with an if-statement if title is missing

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -2,7 +2,12 @@ const axios = require("axios");
 
 
 
+
 const searchMovies = async (req, res) => {
     const title = req.query.title
 
+    if (!title) {                                                                     // if the title query param is missing
+        return res.status(400).json({ error: "Title query parameter is required"})
+        
+    }
 }
