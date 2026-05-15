9e2f094ddc77df2d5be2011e94bd0252e5ef85da
uries242 <teykoa@gmail.com>
2026-05-15 01:34:42 -0400

    Added try-catch for error handling, created get requestusing Axios to the OMDB API

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,13 +1,22 @@
-const axios = require("axios");			
-
-
-
+const axios = require("axios");
 
 const searchMovies = async (req, res) => {
-    const title = req.query.title
+  const title = req.query.title;
 
-    if (!title) {                                                                     // if the title query param is missing
-        return res.status(400).json({ error: "Title query parameter is required"})
-        
-    }
-}
+  if (!title) {
+    // if the title query param is missing
+    return res.status(400).json({ error: "Title query parameter is required" });
+  }
+
+  try {
+    const response = await axios.get("http://www.omdbapi.com/", {
+      params: {
+        s: title,
+        apikey: process.env.OMDB_API_KEY,
+      },
+    });
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: "Something went wrong" });
+  }
+};
